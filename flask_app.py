3980e16bd976b822e89496e2ebd16b4dfc5e143e
Daniel Goodman <camp@goodes.net>
2017-07-18 20:51:17 +0300

    day1-initial

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 app = Flask(__name__)
+app.config['SECRET_KEY'] = b'2782kjh2kjsy89211'
 
 @app.route('/')
 def index():
     return render_template('main.html', the_heading="Hello",
-            items=['a', 'second', '7', 'item4'])
+            items=['a', 'second', '7', 'item4'],
+            session=session)
 
 @app.route('/start')
 def start():
@@ -13,4 +15,5 @@ def start():
 @app.route('/choose/<letter>')
 def choose(letter):
     # show the user profile for that user
+    session['letter'] = letter
     return 'The letter you choose is %s' % letter
