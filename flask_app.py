6ebf89080971cc5dbf5fc8728ff160e172b77cce
Daniel Goodman <camp@goodes.net>
2017-07-18 20:40:45 +0300

    day_1_3

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Here is a new line!'
+    return render_template('main.html')
 
 @app.route('/start')
 def start():
     return 'Lets start a new game!'
+
+@app.route('/choose/<letter>')
+def choose(letter):
+    # show the user profile for that user
+    return 'The letter you choose is %s' % letter
