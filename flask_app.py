1dc95ae6d1ff6f41f33ddd7260f683faaf08b6e7
Daniel Goodman <camp@goodes.net>
2017-07-19 09:41:09 +0300

    v1

--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,20 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask, render_template, session
+import game
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = b'2782kjh2kjsy89211'
+app.config['SECRET_KEY']=b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
-    return render_template('main.html', the_heading="Hello",
-            items=['a', 'second', '7', 'item4'],
-            session=session)
+    session['state'] = game.reset_state(game.choose_word())
+    return render_template('main.html', state=session['state'])
 
-@app.route('/start')
-def start():
-    return 'Lets start a new game!'
-
-@app.route('/choose/<letter>')
-def choose(letter):
-    # show the user profile for that user
-    session['letter'] = letter
-    return 'The letter you choose is %s' % letter
+@app.route('/turn/<letter>')
+def next_turn(letter):
+    if 'state' not in session:
+        session['state'] = game.reset_state(game.choose_word())
+    session['state'] = game.turn(session['state'], letter)
+    return render_template('main.html', state=session['state'])
